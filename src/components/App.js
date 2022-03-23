d7c86a32cecc580d62b35286d4e44cd54a8c85c6
DESKTOP-A7B1IJA\ml <kmr1132@naver.com>
2022-03-23 23:14:19 +0900

    2022.03.23 setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
 import React from "react";
+import AppRouter from "./Router";
 
 function App() {
-  return (
-    <div>
-      <li></li>
-    </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
